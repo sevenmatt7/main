48e01fb4f0a6f177a728a452b8d13b1998203eb8
sevenmatt7 <sevenmatt7@gmail.com>
2019-10-20 00:58:27 +0800

    Completed part 1 of GUI tut

--- src/main/java/com/algosenpai/app/Launcher.java
+++ src/main/java/com/algosenpai/app/Launcher.java
@@ -8,6 +8,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(SceneController.class, args);
+        Application.launch(GUI.class, args);
     }
 }
