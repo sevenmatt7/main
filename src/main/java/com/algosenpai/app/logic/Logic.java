e2c0bfb5e61f9cca7e296a71ae22b78bb62f526c
sevenmatt7 <sevenmatt7@gmail.com>
2019-10-29 11:34:35 +0800

    Added coveralls

--- src/main/java/com/algosenpai/app/logic/Logic.java
+++ src/main/java/com/algosenpai/app/logic/Logic.java
@@ -31,7 +31,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.ArrayList;
 
 public class Logic {
-
     private Parser parser;
     private UserStats userStats;
     private QuizGenerator quizMaker;
