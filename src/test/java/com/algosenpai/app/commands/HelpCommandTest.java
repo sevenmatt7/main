6c1f8a3f2d4168bee3d31a6d9a72e2267a7a383c
sevenmatt7 <sevenmatt7@gmail.com>
2019-11-11 12:59:08 +0800

    Modified help command test

--- src/test/java/com/algosenpai/app/commands/HelpCommandTest.java
+++ src/test/java/com/algosenpai/app/commands/HelpCommandTest.java
@@ -74,7 +74,7 @@ public class HelpCommandTest extends ApplicationTest {
         DialogBox dialogBox = (DialogBox) container.getChildren().get(2);
         String actualText = dialogBox.getDialog().getText();
         Assertions.assertEquals("Try solving these problems on Kattis:\n"
-                + "cups, lineup, mjehuric, sidewayssorting", actualText);
+                + "lineup, mjehuric, sidewayssorting", actualText);
     }
 
     @Test
@@ -84,7 +84,7 @@ public class HelpCommandTest extends ApplicationTest {
         DialogBox dialogBox = (DialogBox) container.getChildren().get(2);
         String actualText = dialogBox.getDialog().getText();
         Assertions.assertEquals("Try solving these problems on Kattis:\n"
-                + "coconut, integerlists, joinstrings", actualText);
+                + "evenup, pairingsocks, coconut", actualText);
     }
 
     @Test
